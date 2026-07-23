7506cbd9ab67bd2c6b3525b3c0a7f99da4317476
wiktor <ryussexd@gmail.com>
2026-07-23 18:02:01 +0200

    feat: add enemy animations

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,41 +6,129 @@ public class EnemyMovement : MonoBehaviour
     public GameObject player;
     private bool hasLOS = false;
     private Rigidbody2D rb;
-
-    private float distance;
-    private Vector2 lastSeen;
-    void Start()
+    public enum EnemyState
     {
-        lastSeen = transform.position;
+        Idle,
+        Chase,
+        Return,
+        LastSeen,
     }
 
-    // Update is called once per frame
+    [SerializeField] private EnemyState CurrentState;
+
+    [SerializeField]private float rangeDistance = 5f;
+    private float distance;
+    private Vector2 lastSeen;
+    private Vector3 spawnLocation;
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        spawnLocation = transform.position;
+        lastSeen = transform.position;
+        CurrentState = EnemyState.Idle;
+    }
+
+    
     void Update()
     {
-        if (hasLOS == true)
+        Debug.DrawRay(
+            transform.position,
+            (player.transform.position - transform.position).normalized * rangeDistance,
+            Color.red
+        );
+        switch (CurrentState)
         {
-            distance = Vector2.Distance(transform.position, player.transform.position);
-            lastSeen = player.transform.position;
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Chase:
+                Chase();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.LastSeen:
+                LastSeen();
+                break;
+        }
+    }
+
+    private void MoveTowardsTarget(Vector2 target)
+    {
         
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        transform.position = Vector2.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+        // animator.SetFloat("MoveX",  direction.x);
+        // animator.SetFloat("MoveY", direction.y);
+    }
+
+    void CanSeePlayer()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, rangeDistance);
+        hasLOS = hit.collider != null && hit.collider.CompareTag("Player");
+    }
+
+    private void Idle()
+    {
+        CanSeePlayer();
+        if (hasLOS)
+        {
+            lastSeen = player.transform.position;
+            CurrentState = EnemyState.Chase;
+        }
+    }
+
+    private void Chase()
+    {
+        CanSeePlayer();
+        if (hasLOS)
+        {
+            lastSeen = player.transform.position;
+            MoveTowardsTarget(player.transform.position);
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, lastSeen, moveSpeed * Time.deltaTime);
+            CurrentState = EnemyState.LastSeen;
         }
     }
 
-    void FixedUpdate()
+    private void LastSeen()
     {
-        RaycastHit2D hit  = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
-        
-        if (hit.collider.name == player.name)
+        CanSeePlayer();
+        if (hasLOS)
         {
-            hasLOS = true;
+            CurrentState = EnemyState.Chase;
+            return;
         }
-        else 
+
+        if (Vector2.Distance(transform.position, lastSeen) > 0.05f)
         {
-            hasLOS = false;
+            Debug.Log("going to last seen");
+            MoveTowardsTarget(lastSeen);
+        }
+        else
+        {
+            CurrentState = EnemyState.Return;
         }
     }
+
+    private void Return()
+        {
+            CanSeePlayer();
+            if (hasLOS)
+            {
+                CurrentState = EnemyState.Chase;
+                return;
+            }
+
+            if (Vector2.Distance(transform.position, spawnLocation) > 0.05f)
+            {
+                Debug.Log("returning to spawn location");
+                MoveTowardsTarget(spawnLocation);
+            }
+            else
+            {
+                CurrentState = EnemyState.Idle;
+            }
+        }
 }
